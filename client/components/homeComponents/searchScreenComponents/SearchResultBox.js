5f0b2f74f1d8c27db097feaa87cc5f3b6b2bff24
erlangamara <erlangamara@gmail.com>
2020-12-08 13:50:25 +0700

    1. Created user search

--- client/components/homeComponents/searchScreenComponents/SearchResultBox.js
+++ client/components/homeComponents/searchScreenComponents/SearchResultBox.js
@@ -2,32 +2,105 @@ import React from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity
 } from 'react-native';
+import { normal } from '../../../assets/FontSize';
+
+//Icon
+import TickIcon from '../../../assets/publicAssets/tickIcon.png';
 
 const SearchResultBox = props => {
   const {
-    searchInput
+    searchInput,
+    category,
+    userResults,
+    navigation
   } = props;
+  
+  const renderUserList = itemData => {
+    return (
+      <TouchableOpacity onPress={() => {
+        navigation.navigate('UserProfile', {
+          userId: itemData.item.id
+        });
+      }} style={styles.userContainerStyle}>
+        <Image source={{uri: itemData.item.profile_photo_path}} style={styles.profileImageStyle} />
+        <Text style={styles.userNameStyle}>{itemData.item.name}</Text>
+        {
+          itemData.item.type !== 0 && <Image source={TickIcon} style={styles.iconStyle} />
+        }
+      </TouchableOpacity>
+    );
+  };
+
+  const ShowList = data => {
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderUserList}
+      />
+    )
+  };
 
   return (
-    <View style={styles.searchResultBoxContainerStyle}>
-      <Text style={styles.searchInputTextStyle}>Search results for <Text style={{fontWeight: "bold"}}>"{searchInput}"</Text></Text>
-    </View>
+    <FlatList
+      ListHeaderComponent={
+        <View style={styles.searchResultBoxContainerStyle}>
+          <Text style={styles.searchInputTextStyle}>Search results for <Text style={{fontWeight: "bold"}}>"{searchInput}"</Text></Text>
+          {
+            category === 'Discussions' ? null : ShowList(userResults)
+          }
+        </View>
+      }
+    />
   );
 };
 
 const styles = StyleSheet.create({
   searchResultBoxContainerStyle: {
     backgroundColor: "#FFFFFF",
-    margin: 8,
-    borderRadius: 8,
-    padding: 16
+    margin: "1.8%",
+    marginBottom: "5%",
+    borderRadius: 8
   },
 
   searchInputTextStyle: {
     color: "#464D60",
-    fontSize: 16
+    fontSize: 16,
+    marginHorizontal: "3%",
+    marginTop: "5%",
+    marginBottom: "2.5%"
+  },
+
+  userContainerStyle: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: "#F5F7F9",
+    padding: "3.8%",
+    alignItems: "center"
+  },
+
+  profileImageStyle: {
+    width: 28,
+    height: 28,
+    borderRadius: 50,
+    marginRight: "3%"
+  },
+
+  userNameStyle: {
+    color: "#464D60",
+    fontFamily: normal,
+    fontSize: 16,
+    marginRight: "1.5%"
+  },
+
+  iconStyle: {
+    width: 15,
+    height: 15
   }
 });
 
---
+++ client/store/reducers/SearchReducer.js
@@ -0,0 +1,18 @@
+const defaultState = {
+  discussions: [],
+  users: []
+};
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'GET_SEARCH':
+      let setDiscussions = action.payload.discussions;
+      let setUsers = action.payload.users;
+      
+      return {...state, discussions: setDiscussions, users: setUsers};
+    default:
+      return state;
+  }
+};
+
+export default reducer;
