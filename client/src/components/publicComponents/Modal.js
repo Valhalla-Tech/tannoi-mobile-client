ee1add6fb7dc4d034df4022c8650a7df484fa5dd
erlangamara <erlangamara@gmail.com>
2021-03-05 11:12:08 +0700

    Working on modal

--- client/src/components/publicComponents/Modal.js
+++ client/src/components/publicComponents/Modal.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Modal } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Modal,
+  Animated,
+} from 'react-native';
 
 const MainModal = (props) => {
   const {
@@ -14,22 +21,46 @@ const MainModal = (props) => {
     disableButton,
   } = props;
 
+  const [resetOpacity, setResetOpacity] = useState(false);
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (openModal) {
+      setResetOpacity(false);
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    } 
+  }, [openModal]);
+
   return (
     <Modal animationType={animation} transparent={true} visible={openModal}>
-      <TouchableOpacity
-        style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.8)' }}
+      <TouchableWithoutFeedback
+        style={{ flex: 1, borderWidth: 10 }}
         onPress={() => {
           closeModal();
+          setResetOpacity(true);
         }}
         disabled={disableButton}>
-        <View
-          style={{ ...styles.modalContainerStyle, ...customContainerStyle }}>
-          <View style={{ ...styles.modalStyle, ...customStyle }}>
-            {children || (child && child())}
-            {modalButton && modalButton()}
+        <View style={{ flex: 1 }}>
+          <Animated.View
+            style={{
+              ...styles.modalBackgroundStyle,
+              opacity: resetOpacity ? 0 : fadeAnim,
+            }}
+          />
+          <View
+            style={{ ...styles.modalContainerStyle, ...customContainerStyle }}>
+            <View style={{ ...styles.modalStyle, ...customStyle }}>
+              {children || (child && child())}
+              {modalButton && modalButton()}
+            </View>
           </View>
         </View>
-      </TouchableOpacity>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
@@ -42,10 +73,18 @@ const styles = StyleSheet.create({
     width: '100%',
   },
 
-  modalContainerStyle: {
+  modalBackgroundStyle: {
     flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+
+  modalContainerStyle: {
     justifyContent: 'center',
     alignItems: 'center',
+    flex: 1,
   },
 
   modalStyle: {
